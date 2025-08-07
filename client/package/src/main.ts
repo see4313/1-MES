3c344edee85ef6e726003ebb8bfd89fffa2c2672
mulumpyo <1119_dh@naver.com>
2025-08-07 15:53:35 +0900

    250807-primevue

--- client/package/src/main.ts
+++ client/package/src/main.ts
@@ -6,9 +6,16 @@ import '@/scss/style.scss';
 import PerfectScrollbar from 'vue3-perfect-scrollbar';
 import VueApexCharts from 'vue3-apexcharts';
 import VueTablerIcons from 'vue-tabler-icons';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
 const app = createApp(App);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
 app.use(router);
 app.use(PerfectScrollbar);
 app.use(VueTablerIcons);
-app.use(VueApexCharts);
 app.use(vuetify).mount('#app');
