3376af8b9b9826288553d166eb46a16f5fc21971
mulumpyo <1119_dh@naver.com>
2025-08-08 12:09:15 +0900

    김대환/생산-지시-등록/2025.08.08

---
+++ client/package/src/components/production/card-content.vue
@@ -0,0 +1,15 @@
+<!-- card-header.vue -->
+<script setup>
+const props = defineProps({
+
+});
+
+</script>
+
+<template>
+  <v-card elevation="10">
+    <v-card-item class="py-6 px-6">
+      <slot name="content"></slot>
+    </v-card-item>
+  </v-card>
+</template>
--- client/package/src/views/production/instruction.vue
+++ client/package/src/views/production/instruction.vue
@@ -1,24 +1,70 @@
-<template>
-    <div class="card">
-        <DataTable :value="products" tableStyle="min-width: 50rem">
-            <Column field="code" header="Code"></Column>
-            <Column field="name" header="Name"></Column>
-            <Column field="category" header="Category"></Column>
-            <Column field="quantity" header="Quantity"></Column>
-        </DataTable>
-    </div>
-</template>
+<!-- instruction.vue -->
 
 <script setup>
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import { ref, onMounted } from 'vue';
-import { ProductService } from '@/service/ProductService';
-
-onMounted(() => {
-    ProductService.getProductsMini().then((data) => (products.value = data));
-});
-
-const products = ref();
-
+import CardHeader from '@/components/production/card-header.vue';
 </script>
+
+<template>
+    <v-card elevation="10">
+        <v-card-item class="py-6 px-6">
+            <CardHeader
+                title="생산 지시"
+                btn-icon="mdi-plus-circle"
+                btn-text="생산 계획 불러오기"
+                btn-variant="flat"
+                btn-color="primary"
+                @btn-click="planLoad"
+            />
+              <v-table>
+                    <thead>
+                        <tr>
+                            <th class="text-center font-weight-bold">
+                            품목 번호
+                            </th>
+                            <th class="text-center font-weight-bold">
+                            품목 유형
+                            </th>
+                            <th class="text-center font-weight-bold">
+                            품목명
+                            </th>
+                            <th class="text-center font-weight-bold">
+                            지시 수량
+                            </th>
+                            <th>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center">001</td>
+                            <td class="text-center">테스트 제품</td>
+                            <td class="text-center">테스트</td>
+                            <td class="text-center">
+                            <v-text-field
+                                v-model="quantity"
+                                type="number"
+                                variant="outlined"
+                                hide-details
+                                density="compact"
+                            />
+                            </td>
+                            <td class="text-center">
+                                <v-btn
+                                    color="error"
+                                >
+                                <v-icon>mdi-delete</v-icon>
+                                </v-btn>
+                            </td>
+                        </tr>
+                    </tbody>
+                </v-table>
+                <v-row justify="center" class="my-2">
+                        <v-btn
+                            append-icon="mdi-plus-circle"
+                            color="primary"
+                        >품목 추가
+                    </v-btn>
+                </v-row>
+        </v-card-item>
+    </v-card>
+</template>
