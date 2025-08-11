fb2ddd213e93be9a8feab3ca91426ad89272760c
admin <pc@pc.com>
2025-08-11 17:25:03 +0900

    손이영/영업/주문조회/20250811

--- server/services/order_service.js
+++ server/services/order_service.js
@@ -2,12 +2,44 @@ const mariadb = require("../database/mapper.js");
 // 실제로 제공하는 기능 : 요구사항에 따라 달라짐
 
 // 전체조회
-const findAll = async () => {
-  let list = await mariadb.query("orderList");
+const orderList = async (filters) => {
+  let list = await mariadb.query("orderList", filters);
   return list;
 };
 
-function convertToArray(obj, columns) {
+// 상세조회
+const detailOrder = async (order_id) => {
+  let list = await mariadb.query("detailOrder", order_id);
+  return list;
+};
+
+// 모달(담당자)
+const empModal = async () => {
+  let list = await mariadb.query("empModal");
+  return list;
+};
+
+// 모달(주문)
+const orderModal = async () => {
+  let list = await mariadb.query("orderModal");
+  return list;
+};
+
+//등록
+const orderInsert = async () => {
+  let insertData = orderToArray([
+    "order_name",
+    "emp_id",
+    "vend_id",
+    "ordr_date",
+    "paprd_date",
+    "remk",
+  ]);
+  let resInfo = await mariadb.query("orderInsert", insertData);
+  return resInfo;
+};
+
+function orderToArray(obj, columns) {
   let result = [];
   for (let column of columns) {
     result.push(obj[column]);
@@ -15,19 +47,10 @@ function convertToArray(obj, columns) {
   return result;
 }
 
-//등록
-const addNew = async (boardInfo) => {
-  let insertData = convertToArray(boardInfo, [
-    "order_id",
-    "emp_id",
-    "ordr_date",
-    "paprd_date",
-    "remk",
-  ]);
-  let resInfo = await mariadb.query("orderInsert", insertData);
-};
-
 module.exports = {
-  findAll,
-  addNew,
+  orderList,
+  orderInsert,
+  empModal,
+  orderModal,
+  detailOrder,
 };
