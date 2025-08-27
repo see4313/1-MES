1be01ea559ce83d42a8683b28d84653f5ec9eebb
gywns <gywns8339@naver.com>
2025-08-27 11:48:17 +0900

    25.08.27

--- client/package/src/layouts/full/Topbar.vue
+++ client/package/src/layouts/full/Topbar.vue
@@ -2,12 +2,10 @@
 import { Icon } from '@iconify/vue';
 </script>
 <template>
-    <div class="w-full py-4 px-6 w-100" style="background-color: #f2e8d1;">
+    <div class="w-full py-4 px-6 w-100" style="background-color: #f2e8d1">
         <div class="d-flex flex-lg-row flex-column gap-3 justify-space-between align-center">
             <div class="d-flex align-center ga-6">
-                <a href="/" target="_blank" class="lh-0">
-                    <img src="@/assets/images/logos/logo.svg" width="40"
-                /></a>
+                <a href="/" class="lh-0"> <img src="@/assets/images/logos/logo.svg" width="40" /></a>
             </div>
             <div class="d-flex flex-md-row flex-column align-center ga-4">
                 <div class="d-flex flex-md-row flex-wrap justify-md-end justify-center ga-3"></div>
